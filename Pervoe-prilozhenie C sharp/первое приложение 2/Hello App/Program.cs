0e2a79cd0362e788245586f26e0eb7d79256bf12
aleksandr4082 <83226648+aleksandr4082@users.noreply.github.com>
2022-11-24 17:26:06 +0300

    Add files via upload

---
+++ Pervoe-prilozhenie C sharp/первое приложение 2/Hello App/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите  своё имя");
+string? name = Console.ReadLine(); // вводим имя
+Console.WriteLine($"Привет {name}"); // выводим имя на консоль
